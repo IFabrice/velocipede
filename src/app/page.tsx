6a26f6ae03a3385519604b52ee64a6985c9ed9c4
Dynamique Twizere <twizeredynamique@gmail.com>
2024-12-08 13:43:49 -0600

    Learning2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-black text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <div>The app that I'm excited to work on!</div>
+        <div>i love bikes!</div>
       </div>
     </main>
   );
