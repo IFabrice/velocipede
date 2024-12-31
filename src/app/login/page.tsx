680db871f1b5a6e88ce5d4bd358262c2e078f1d6
Dynamique Twizere <twizeredynamique@gmail.com>
2024-12-30 19:59:08 -0600

    Changed CSS to Tailwind for Signup page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,13 @@
-import "../../styles/styles.css";
+import "../../styles/globals.css";
 
 function Login() {
   return (
-    <div className="loginPage">
-      <img src="./velocipede.png" alt="Velocipede logo" className="logo" />
-      <h2>Log in to your account</h2>
+    <div className="bg-white-500 flex h-screen flex-col items-center justify-center">
+      <img src="./velocipede.png" alt="Velocipede logo"         className="h-auto w-[170px] relative bottom-[20px]"/>
+      <h1 className="relative bottom-[15px] text-[28px] font-[600] text-[#454545]">Log in to your account</h1>
 
-      <form className="loginForm">
-        <label htmlFor="loginEmail" className="labels">
+      <form id="loginForm" className="flex flex-col justify-center gap-2">
+        <label htmlFor="loginEmail" className="inputLabel">
           Email
         </label>
         <input
@@ -15,12 +15,13 @@ function Login() {
           id="loginEmail"
           placeholder="Enter your email"
           autoComplete="on"
+          className="inputBox"
         ></input>
-        <label htmlFor="loginPassword" className="labels">
+        <label htmlFor="loginPassword" className="inputLabel">
           Password
         </label>
-        <input type="password" id="loginPassword"></input>
-        <a href="abc.com" className="labels forgot">Forgot password</a>
+        <input type="password" id="loginPassword" className="inputBox"></input>
+        <a href="abc.com" className="inputLabel forgot">Forgot password</a>
         <input type="submit" value="Sign in"></input>
         <p className="forgot">
           Don't have an account? <span><a href="abc.com">Sign up</a></span>
